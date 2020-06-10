8b212172e21964b5c926cc5c88916760e71ab52d
Filip Krikava <krikava@gmail.com>
2020-06-10 22:03:40 +0200

    Add scratchpad binding

--- config.def.h
+++ config.def.h
@@ -62,12 +62,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char scratchpadname[] = "dropdown";
+static const char *scratchpadcmd[] = { "/usr/local/bin/st", "-t", scratchpadname, "-g", "132x32", "tmux", "new-session", "-A", "-s", scratchpadname, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-        { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Left,   focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_Right,  focusstack,     {.i = -1 } },
