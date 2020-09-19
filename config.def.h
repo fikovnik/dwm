adf2f04c6eccb2013c86b858b94d69d603642655
Filip Krikava <krikava@gmail.com>
2020-09-19 17:46:50 +0200

    Change font to JetBrainsMono

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVu Sans Mono for Powerline:pixelsize=13:antialias=true:autohint=true" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:pixelsize=13:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#4C566A";
